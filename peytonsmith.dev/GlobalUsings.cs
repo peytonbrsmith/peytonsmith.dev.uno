150f3f8b59b4318b4dcf2a0d8d7fb2042205a531
Peyton Smith <peyton@falkon.net>
2025-02-03 22:04:53 -0600

    removed old references

--- peytonsmith.dev/GlobalUsings.cs
+++ peytonsmith.dev/GlobalUsings.cs
@@ -6,8 +6,6 @@ global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using peytonsmith.dev.Models;
 global using peytonsmith.dev.Presentation;
-global using peytonsmith.dev.DataContracts;
-global using peytonsmith.dev.DataContracts.Serialization;
 global using peytonsmith.dev.Services.Caching;
 global using peytonsmith.dev.Services.Endpoints;
 global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
