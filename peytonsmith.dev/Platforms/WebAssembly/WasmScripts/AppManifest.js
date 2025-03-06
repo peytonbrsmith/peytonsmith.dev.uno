7a03699cc990f5f94741b330d9e04cbe73fe94ec
Peyton Smith <github@peytonsmith.dev>
2025-03-05 18:12:52 -0600

    add light and dark theme background colors to AppManifest.js

--- peytonsmith.dev/Platforms/WebAssembly/WasmScripts/AppManifest.js
+++ peytonsmith.dev/Platforms/WebAssembly/WasmScripts/AppManifest.js
@@ -2,4 +2,6 @@ var UnoAppManifest = {
     displayName: "peytonsmith.dev",
     splashScreenImage: "https://raw.githubusercontent.com/peytonbrsmith/peytonsmith.dev.uno/refs/heads/main/peytonsmith.dev/Assets/Splash/splash_screen.png",
     splashScreenColor: "transparent",
+    lightThemeBackgroundColor: "#ECF2EE",
+    darkThemeBackgroundColor: "#00382C",
 }
